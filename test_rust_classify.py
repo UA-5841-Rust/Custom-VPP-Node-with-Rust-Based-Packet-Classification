b0253b447f3204fcf53b6b30a631f3ee7f98eff1
MykolaShev <smikola25@gmail.com>
2026-09-04 12:00:54 +0000

    refactor: change test cases appearance

--- test_rust_classify.py
+++ test_rust_classify.py
@@ -16,27 +16,27 @@ class TestRustClassify(VppTestCase):
         """ Test Rust Classification Scenarios """
         
         # 1. Valid UDP packet
-        p_valid = (Ether(src="00:11:22:33:44:55", dst="aa:bb:cc:dd:ee:ff") /
-                   IP(src="127.0.0.1", dst="127.0.0.1") /
-                   UDP(sport=1234, dport=5678) /
+        p_valid = (Ether(src="00:11:22:33:44:55", dst="aa:bb:cc:dd:ee:ff"),
+                   IP(src="127.0.0.1", dst="127.0.0.1"),
+                   UDP(sport=1234, dport=5678),
                    Raw(b'hello'))
 
         # 2. IPv4 packet with options (IHL > 5)
-        p_opts = (Ether(src="00:11:22:33:44:55", dst="aa:bb:cc:dd:ee:ff") /
-                  IP(src="127.0.0.1", dst="127.0.0.1", options=['\x00\x00\x00\x00']) /
-                  UDP(sport=1234, dport=5678) /
+        p_opts = (Ether(src="00:11:22:33:44:55", dst="aa:bb:cc:dd:ee:ff"),
+                  IP(src="127.0.0.1", dst="127.0.0.1", options=['\x00\x00\x00\x00']),
+                  UDP(sport=1234, dport=5678),
                   Raw(b'hello'))
 
         # 3. Packet with invalid EtherType (IPv6 ethertype but IPv4 header)
-        p_inv_eth = (Ether(src="00:11:22:33:44:55", dst="aa:bb:cc:dd:ee:ff", type=0x86dd) /
-                     IP(src="127.0.0.1", dst="127.0.0.1") /
-                     UDP(sport=1234, dport=5678) /
+        p_inv_eth = (Ether(src="00:11:22:33:44:55", dst="aa:bb:cc:dd:ee:ff", type=0x86dd),
+                     IP(src="127.0.0.1", dst="127.0.0.1"),
+                     UDP(sport=1234, dport=5678),
                      Raw(b'hello'))
 
         # 4. Truncated UDP payload (Forcing IP len to be larger than actual packet)
-        p_trunc = (Ether(src="00:11:22:33:44:55", dst="aa:bb:cc:dd:ee:ff") /
-                   IP(src="127.0.0.1", dst="127.0.0.1", len=100) /
-                   UDP(sport=1234, dport=5678, len=80) /
+        p_trunc = (Ether(src="00:11:22:33:44:55", dst="aa:bb:cc:dd:ee:ff"),
+                   IP(src="127.0.0.1", dst="127.0.0.1", len=100),
+                   UDP(sport=1234, dport=5678, len=80),
                    Raw(b'short'))
 
         # Inject packets directly into our node using Scapy's hex conversion
